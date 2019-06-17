e947c4dba6c49c8ef24dee852b410afd98382ffd
Zavier <zmyles@comcast.net>
2019-06-17 14:36:08 -0400

    start of game

---
+++ Test-Game.py
@@ -0,0 +1,31 @@
+# Author:   Zavier P. Myles
+
+
+# Purpose: Testing my Know knowlegde and skills with Python.
+#          Creating a small little game from what I know
+
+
+
+
+def start():
+    print(get_number())
+
+def get_number():
+    number = 12
+    return number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    start()
